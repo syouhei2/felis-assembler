868d095b282500b030a42855a5f26d47a911ce34
syouhei2 <didibarks@yahoo.co.jp>
2016-11-08 15:19:49 +0900

    ..

--- fib.s
+++ fib.s
@@ -1,14 +1,17 @@
 main:
-  addi $r2, $r0, 10
+  addi $r2, $r0, 2
   jal fib
   halt
 
 
 fib:
-  addi $r30, $r30, 12
+  addi $r30, $r30, 16
 
   sw $ra, $r30, -12
   sw $r2, $r30, -8
+  sw $r4, $r30, -4
+
+  beq $r2, $r0, 12
 
   addi $r3, $r2, -1
   bgtz $r3, 8
@@ -16,21 +19,23 @@ fib:
 
   addi $r2, $r2, -1
   jal fib
-  move $r3, $r1
+  sw $r1, $r30, -16
 
   addi $r2, $r2, -1
   jal fib
 
-  add $r1, $r1, $r3
+  lw $r4, $r30, -16
+  add $r1, $r1, $r4
 
 
   lw $ra, $r30, -12
   lw $r2, $r30, -8
-  addi $r30, $r30, -12
+  lw $r4, $r30, -4
+  addi $r30, $r30, -16
   jr $ra 
 
 rtn1:
   addi $r1, $r0, 1
-  addi $r30, $r30, -12
+  addi $r30, $r30, -16
   jr $ra
 
