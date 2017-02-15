f2f73233d6bc28cdfa391a3d421df8605e30a2a0
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2017-02-16 03:18:24 +0900

    Modify remove_no_effect.py

--- remove_no_effect.py
+++ remove_no_effect.py
@@ -5,33 +5,26 @@ def addi_0(l):
     return l[0] == "addi" and l[1] == l[2] and int(l[3]) == 0
 
 
-def divi_1(l):
-    return l[0] == "divi" and l[1] == l[2] and int(l[3]) == 1
-
-
-def mult_1(l):
-    return l[0] == "mult" and l[1] == l[2] and int(l[3]) == 1
-
-
 def mov_same(l):
     return l[0] == "mov" and l[1] == l[2]
 
-
-def mov_s_same(l):
-    return l[0] == "mov_s" and l[1] == l[2]
-
-remove_funcs = [addi_0, divi_1, mult_1, mov_same, mov_s_same]
+remove_funcs = [addi_0, mov_same]
+remove_cnt = {f.__name__: 0 for f in remove_funcs}
 
 f_in = open(sys.argv[1])
 f_out = open(sys.argv[1].replace('.s', '_removed.s'), 'w')
-cnt = 0
 for l in f_in:
     l2 = l.split()
     if len(l2) == 0:
         continue
-    if any([f(l2) for f in remove_funcs]):
-        print("removed", l, end="")
-        cnt += 1
+    removed = False
+    for f in remove_funcs:
+        if f(l2):
+            print("removed", l, end="")
+            remove_cnt[f.__name__] += 1
+            removed = True
+            break
+    if removed:
         continue
     f_out.write(l)
-print(cnt, "instructions removed")
+print(remove_cnt, "instructions removed")
